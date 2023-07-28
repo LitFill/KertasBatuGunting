bd5f867b8378b8db3072131a385dd2a493e0b03a
LitFill <98679748+LitFill@users.noreply.github.com>
2023-07-28 16:30:53 +0700

    format, if -> switch, refactor, fix reset confrmtn

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,31 @@
-const skor1 = JSON.parse(localStorage.getItem('skor1')) || {
+const skor1 = JSON.parse(localStorage.getItem("skor1")) || {
     menang: 0,
     kalah: 0,
     seri: 0,
 };
 
+const tombolKertas = document.querySelector(".js-tombol-kertas");
+const tombolBatu = document.querySelector(".js-tombol-batu");
+const tombolGunting = document.querySelector(".js-tombol-gunting");
+const tombolReset = document.querySelector(".js-tombol-reset");
+const tombolAuto = document.querySelector(".js-tombol-auto");
+const divKonfirmasi = document.querySelector(".konfirmasi");
+
+tombolKertas.addEventListener("click", () => mainkanGame("kertas"));
+tombolBatu.addEventListener("click", () => mainkanGame("batu"));
+tombolGunting.addEventListener("click", () => mainkanGame("gunting"));
+tombolReset.addEventListener("click", () => showConfirmation(true));
+tombolAuto.addEventListener("click", () => autoPlay());
+
 function pilihPilihanKonputer() {
     const angkaAcak = Math.random();
     let pilihanKomputer;
-    if (angkaAcak < 1/3) {
-        pilihanKomputer = 'kertas';
-    } else if (angkaAcak < 2/3) {
-        pilihanKomputer = 'batu'
+    if (angkaAcak < 1 / 3) {
+        pilihanKomputer = "kertas";
+    } else if (angkaAcak < 2 / 3) {
+        pilihanKomputer = "batu";
     } else {
-        pilihanKomputer = 'gunting'
+        pilihanKomputer = "gunting";
     }
     return pilihanKomputer;
 }
@@ -26,8 +39,10 @@ function mainkanGame(pilihanPemain) {
     perbaruiElemenHasil(hasil);
     perbaruiElemenSkor();
 
-    localStorage.setItem('skor1', JSON.stringify(skor1));
+    localStorage.setItem("skor1", JSON.stringify(skor1));
     // alert(`Kamu memilih ${pilihanPemain}.\nKomputer memilih ${pilihanKomputer}.\nHasilnya ${hasil}\nMenang: ${skor1.menang}, Kalah: ${skor1.kalah}, Seri: ${skor1.seri}.`);
+
+    showConfirmation(false);
 }
 
 perbaruiElemenSkor();
@@ -39,7 +54,7 @@ function perbaruiElemenPilihan(pilihanPemain, pilihanKomputer) {
     } else if (pilihanPemain === "batu") {
         plhnpmn = "rock";
     } else {
-        plhnpmn = "scissors"
+        plhnpmn = "scissors";
     }
     let plhkmp;
     if (pilihanKomputer === "kertas") {
@@ -47,39 +62,43 @@ function perbaruiElemenPilihan(pilihanPemain, pilihanKomputer) {
     } else if (pilihanKomputer === "batu") {
         plhkmp = "rock";
     } else {
-        plhkmp = "scissors"
+        plhkmp = "scissors";
     }
-    document.querySelector('.js-pilihan').innerHTML = `Kamu memilih: <img class="ikon-pilihan" src="images/${plhnpmn}-emoji.png"><br>Komputer memilih: <img class="ikon-pilihan" src="images/${plhkmp}-emoji.png"><br><br><img class="ikon-pilihan" src="images/${plhnpmn}-emoji.png">vs<img class="ikon-pilihan" src="images/${plhkmp}-emoji.png">`;
+    document.querySelector(
+        ".js-pilihan"
+    ).innerHTML = `Kamu memilih: <img class="ikon-pilihan" src="images/${plhnpmn}-emoji.png"><br>Komputer memilih: <img class="ikon-pilihan" src="images/${plhkmp}-emoji.png"><br><br><img class="ikon-pilihan" src="images/${plhnpmn}-emoji.png">vs<img class="ikon-pilihan" src="images/${plhkmp}-emoji.png">`;
 }
 
 function perbaruiElemenHasil(hasil) {
-    document.querySelector('.js-hasil').innerHTML = `Hasilnya: ${hasil}`;
+    document.querySelector(".js-hasil").innerHTML = `Hasilnya: ${hasil}`;
 }
 
 function perbaruiElemenSkor() {
-    document.querySelector('.js-skor').innerHTML = `Menang: ${skor1.menang} | Kalah: ${skor1.kalah} | Seri: ${skor1.seri}`;
+    document.querySelector(
+        ".js-skor"
+    ).innerHTML = `Menang: ${skor1.menang} | Kalah: ${skor1.kalah} | Seri: ${skor1.seri}`;
 }
 
 function tentukanHasil(pilihanPemain, pilihanKomputer) {
     if (pilihanPemain === pilihanKomputer) {
-        return 'seri.';
+        return "seri.";
     } else if (
-        (pilihanPemain === 'kertas' && pilihanKomputer === 'batu') ||
-        (pilihanPemain === 'batu' && pilihanKomputer === 'gunting') ||
-        (pilihanPemain === 'gunting' && pilihanKomputer === 'kertas')
+        (pilihanPemain === "kertas" && pilihanKomputer === "batu") ||
+        (pilihanPemain === "batu" && pilihanKomputer === "gunting") ||
+        (pilihanPemain === "gunting" && pilihanKomputer === "kertas")
     ) {
-        return 'kamu menang!';
+        return "kamu menang!";
     } else {
-        return 'kamu kalah.';
+        return "kamu kalah.";
     }
 }
 
 function perbaruiSkor(hasil) {
-    if (hasil === 'kamu menang!') {
+    if (hasil === "kamu menang!") {
         skor1.menang += 1;
-    } else if (hasil === 'kamu kalah.') {
+    } else if (hasil === "kamu kalah.") {
         skor1.kalah += 1;
-    } else if (hasil === 'seri.') {
+    } else if (hasil === "seri.") {
         skor1.seri += 1;
     }
 }
@@ -89,7 +108,7 @@ function resetSkor() {
     skor1.kalah = 0;
     skor1.seri = 0;
     perbaruiElemenSkor();
-    localStorage.removeItem('skor1');
+    localStorage.removeItem("skor1");
     // alert(`Skor telah direset.\nMenang: ${skor1.menang}, Kalah: ${skor1.kalah}, Seri: ${skor1.seri}.`);
     showConfirmation(false);
 }
@@ -109,24 +128,11 @@ function autoPlay() {
         clearInterval(intervalId);
         // isAutoPlay = !isAutoPlay;
     }
-    if (isAutoPlay) document.querySelector('.js-tombol-auto').innerText = 'Stop Auto';
-    else document.querySelector('.js-tombol-auto').innerText = 'Auto Play';
+    if (isAutoPlay) tombolAuto.innerText = "Stop Auto";
+    else tombolAuto.innerText = "Auto Play";
+
+    showConfirmation(false);
 }
-
-const tombolKertas = document.querySelector('.js-tombol-kertas');
-tombolKertas.addEventListener('click', () => mainkanGame('kertas'));
-
-const tombolBatu = document.querySelector('.js-tombol-batu');
-tombolBatu.addEventListener('click', () => mainkanGame('batu'));
-
-const tombolGunting = document.querySelector('.js-tombol-gunting');
-tombolGunting.addEventListener('click', () => mainkanGame('gunting'));
-
-const tombolReset = document.querySelector('.js-tombol-reset');
-tombolReset.addEventListener('click', () => showConfirmation(true));
-
-const tombolAuto = document.querySelector('.js-tombol-auto');
-tombolAuto.addEventListener('click', () => autoPlay());
 
 // const tombolYa = document.querySelector('.js-tombol-ya');
 // tombolYa.addEventListener('click', () => resetSkor());
@@ -134,32 +140,37 @@ tombolAuto.addEventListener('click', () => autoPlay());
 // const tombolTidak = document.querySelector('.js-tombol-tidak');
 // tombolTidak.addEventListener('click', () => showConfirmation(false));
 
-document.body.addEventListener('keydown', (event) => {
-    if (event.key === 'k') {
-        mainkanGame('kertas');
-    } else if (event.key === 'b') {
-        mainkanGame('batu');
-    } else if (event.key === 'g') {
-        mainkanGame('gunting');
-    } else if (event.key === 'r') {
-        resetSkor();
-    } else if (event.key === 'Backspace') {
-        resetSkor();
-    } else if (event.key === 'a') {
-        autoPlay();
-    } else {
-        console.log(event.key);
+document.body.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "k":
+            mainkanGame("kertas");
+            break;
+        case "b":
+            mainkanGame("batu");
+            break;
+        case "g":
+            mainkanGame("gunting");
+            break;
+        case "r":
+        case "Backspace":
+            resetSkor();
+            break;
+        case "a":
+            autoPlay();
+            break;
+
+        default:
+            console.log(event.key);
     }
 });
 function showConfirmation(bool) {
     if (bool === true) {
-        document.querySelector('.konfirmasi')
-        .innerHTML = `
+        divKonfirmasi.innerHTML = `
         <p>Apakah Anda yakin untuk mereset skor?</p>
         <button class="tombol-konfirmasi js-tombol-ya" onclick="resetSkor()">Ya</button>
         <button class="tombol-konfirmasi js-tombol-tidak" onclick="showConfirmation(false)">Tidak</button>
         `;
     } else if (bool === false) {
-        document.querySelector('.konfirmasi').innerHTML = '';
+        divKonfirmasi.innerHTML = "";
     }
 }
